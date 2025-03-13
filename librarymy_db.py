99f081b5ac041d1d3be2ec87eaee89e5867c7402
OtajonG <gafforovotajon019@gmail.com>
2025-03-13 17:57:37 +0800

    Corrected .gitignore

---
+++ librarymy_db.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+try:
+    import sqlite3
+    print("sqlite3 module is available.")
+    print(f"SQLite version: {sqlite3.sqlite_version}")
+except ImportError:
+    print("sqlite3 module is not available.")
