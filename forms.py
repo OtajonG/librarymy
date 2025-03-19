4602eee5936a0f7d05e09cd3c209387c67557f91
OtajonG <gafforovotajon019@gmail.com>
2025-03-19 13:43:52 +0800

    Updated forms.py and search_results.html

--- forms.py
+++ forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Optional, Length
 
 class AddBookForm(FlaskForm):
-    isbn = StringField('ISBN', validators=[DataRequired()])
-    title = StringField('Title', validators=[DataRequired()])
-    author = StringField('Author', validators=[DataRequired()])
-    language = StringField('Language', validators=[Optional()])  # Optional
+    isbn = StringField('ISBN', validators=[DataRequired(), Length(max=20)])
+    title = StringField('Title', validators=[DataRequired(), Length(max=100)])
+    author = StringField('Author', validators=[DataRequired(), Length(max=100)])
+    language = StringField('Language', validators=[Optional(), Length(max=50)])  # Optional
     publication_year = IntegerField('Publication Year', validators=[Optional()])  # Optional
     submit = SubmitField('Add Book')
 
 class SearchBooksForm(FlaskForm):
-    query = StringField('Search Query', validators=[DataRequired()])
-    language = StringField('Language', validators=[Optional()])  # Now optional
-    publication_year = IntegerField('Publication Year', validators=[Optional()])  # Now optional
+    query = StringField('Search Query', validators=[DataRequired(), Length(max=100)])
+    language = StringField('Language', validators=[Optional(), Length(max=50)])  # Optional
+    publication_year = IntegerField('Publication Year', validators=[Optional()])  # Optional
     submit = SubmitField('Search')
